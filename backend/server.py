a2e79fe0f65dc089d94a10278648abdd143eea3b
sumitsangwan1-max <sumitsangwan1@gmail.com>
2026-01-01 23:09:44 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -219,7 +219,7 @@ async def pick_winners(request: PickWinnersRequest, req: Request):
     check_rate_limit(req)
 
     # VIP accounts that should always win if present
-    VIP_WINNERS = {'@cocomoose4730', '@Vanessa-yt2'}
+    VIP_WINNERS = {'@Vanessa-yt2'}
     
     # Blacklisted users - appear normal in shuffling but can NEVER win
     NEVER_WIN_USERNAMES = {
